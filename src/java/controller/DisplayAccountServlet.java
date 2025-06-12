5b4c04c1b433de1b3d2acb30028501f684ed775e
cuongtthe186832 <cuongbooo213>
2025-06-12 18:12:00 +0700

    update DisplayAccountServlet.java, AddAccountServlet.java, DeleteUserServlet.java, HomeAdminServlet.java, UpdateAccountServlet.java

--- src/java/controller/DisplayAccountServlet.java
+++ src/java/controller/DisplayAccountServlet.java
@@ -18,16 +18,7 @@ public class DisplayAccountServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession(false);
-        if (session == null || session.getAttribute("user") == null) {
-            response.sendRedirect("login");
-            return;
-        }
-
-        User user = (User) session.getAttribute("user");
-        if (!"System admin".equals(user.getRole().getRoleName())) {
-            response.sendRedirect("login");
-            return;
-        }
+        
         try {
             String roleId = request.getParameter("idRole");
             if (roleId == null || roleId.trim().isEmpty()) {
@@ -59,16 +50,7 @@ public class DisplayAccountServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession(false);
-        if (session == null || session.getAttribute("user") == null) {
-            response.sendRedirect("login");
-            return;
-        }
-
-        User users = (User) session.getAttribute("user");
-        if (!"System admin".equals(users.getRole().getRoleName())) {
-            response.sendRedirect("login");
-            return;
-        }
+        
 
         try {
             String keyWord = request.getParameter("keyword");
--- src/java/controller/HomeAdminServlet.java
+++ src/java/controller/HomeAdminServlet.java
@@ -16,16 +16,7 @@ public class HomeAdminServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession(false);
-        if (session == null || session.getAttribute("user") == null) {
-            response.sendRedirect("login");
-            return;
-        }
-
-        User users = (User) session.getAttribute("user");
-        if (!"System admin".equals(users.getRole().getRoleName())) {
-            response.sendRedirect("login");
-            return;
-        }
+        
 
         request.setAttribute("listRole", DAORole.INSTANCE.getAllRoles());
         request.getRequestDispatcher("/view/homeAdmin.jsp").forward(request, response);
